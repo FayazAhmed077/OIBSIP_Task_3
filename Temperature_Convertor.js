963003b1d73e69002f06bbe6c024fcf0b65a4dd5
FayazAhmed077 <126765533+FayazAhmed077@users.noreply.github.com>
2023-05-01 21:44:56 +0530

    JavaScript Code

---
+++ Temperature_Convertor.js
@@ -0,0 +1,18 @@
+const convertBtn = document.getElementById("convert-btn");
+const temperatureInput = document.getElementById("temperature");
+const unitSelect = document.getElementById("unit");
+const resultDiv = document.getElementById("result");
+
+convertBtn.addEventListener("click", () => {
+  const temperature = temperatureInput.value;
+  const unit = unitSelect.value;
+  let convertedTemperature;
+  
+  if (unit === "celsius") {
+    convertedTemperature = temperature * 1.8 + 32;
+  } else if (unit === "fahrenheit") {
+    convertedTemperature = (temperature - 32) / 1.8;
+  }
+  
+  resultDiv.innerHTML = `${temperature}&deg; ${unit} is ${convertedTemperature.toFixed(2)}&deg; ${unit === "celsius" ? "Fahrenheit" : "Celsius"}`;
+});
